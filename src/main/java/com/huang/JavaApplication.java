ea8de7fc5a15b262100b985d5e0017eebbaaa94a
Huang-it-dot <nan1220111@gmail.com>
2024-11-08 23:14:10 +0900

    huang.baihui
    version1.0 mod by guan

--- src/main/java/com/huang/JavaApplication.java
+++ src/main/java/com/huang/JavaApplication.java
@@ -6,11 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-@MapperScan("com.huang.Mapper")
-public class JavaSpringLearnApplication {
+
+public class JavaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaSpringLearnApplication.class, args);
+		SpringApplication.run(JavaApplication.class, args);
 	}
 
 }
--- src/main/java/com/huang/controller/DemoController.java
+++ src/main/java/com/huang/controller/DemoController.java
@@ -14,7 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.huang.Form.MyForm;
 import com.huang.Model.User;
-import com.huang.Service.UserService;
+
 
 import jakarta.validation.Valid;
 
@@ -26,13 +26,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class DemoController implements WebMvcConfigurer{
 	
-//	@Autowired
-//	private MessageSource messageSource;
-//	
-//	@Autowired
-//	private UserService userService;
-	
-	@GetMapping("/Demo1")
+@GetMapping("/")
 //	@ResponseBody
 	public String firstMethod(Model model) {
 		model.addAttribute("name", "yuyu");
@@ -42,27 +36,5 @@ public class DemoController implements WebMvcConfigurer{
 //		model.addAttribute("user", user);
 //        return "UserResult";
 	}
-//	
-//	@Override
-//	public void addViewControllers(ViewControllerRegistry registry) {
-//		registry.addViewController("/Results").setViewName("Results");
-//	}
-//	
-//	@GetMapping("/")
-//	public String showForm(MyForm test) {
-//		return "MyForm";
-//	}
-//	
-//	@PostMapping("/")
-////	public String checkPersonInfo(@Valid MyForm myForm, BindingResult bindingResult) {
-//	public String checkPersonInfo(@Valid @ModelAttribute MyForm myForm, BindingResult bindingResult, Model model) {
-////		model.addAttribute("nameText", messageSource.getMessage("nameText", null, Locale.getDefault()));
-//		if (bindingResult.hasErrors()) {
-//			return "MyForm";
-//		}
-//		
-//		return "redirect:/Results";
-//	}
-//	
-	
+
 }
