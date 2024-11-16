d07c05813c7949b3f60008db2cc1741252cdc614
Huang-it-dot <nan1220111@gmail.com>
2024-11-16 17:34:42 +0900

    loginのCSS追加

--- src/main/resources/static/js/main.js
+++
@@ -1,8 +0,0 @@
-// JavaScript for Delete Button
-function deleteUser() {
-    const confirmation = confirm("Are you sure you want to delete this user?");
-    if (confirmation) {
-        // Redirect to delete action
-        window.location.href = '/deleteUser?id=12345';
-    }
-}
