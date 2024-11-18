aabb3b58725ba6b1fc7a2d174d858d6f4390658a
Huang-it-dot <nan1220111@gmail.com>
2024-11-18 12:03:10 +0900

    コメント追加111812

--- src/main/java/com/huang/Controller/PageController.java
+++ src/main/java/com/huang/Controller/PageController.java
@@ -7,22 +7,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class PageController {
 
-    // href="/"跳转页面
-	//访问根路径时跳转到 index.html
+    // href="/"で画面遷移
+	//index.htmlに遷移
     @GetMapping("/")
     public String index() {
-        return "hello"; // 返回视图名称 "index" -> src/main/resources/templates/hello.html
+        return "hello"; 
     }
 
-    // 表单提交后跳转到 success.html
+    //success.htmlに遷移
     @PostMapping("/submit")
     public String submitForm() {
-        return "success"; // 返回视图名称 "success" -> src/main/resources/templates/success.html
+        return "success"; 
     }
-    // window.location.href = "/success";跳转页面
+    // window.location.href で画面遷移（HTMLにJavaScriptをインポートする）
     @GetMapping("/success")
     public String showSuccessPage() {
-        // 返回视图名称，对应的模板文件为 src/main/resources/templates/success.html
         return "success";
     }
 }
