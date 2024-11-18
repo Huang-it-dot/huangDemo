5d5d5888bf4b168d8e2d9182bbf3254ef266e6e2
Huang-it-dot <nan1220111@gmail.com>
2024-11-18 11:51:58 +0900

    window.location.href 使用して画面遷移成功

--- src/main/java/com/huang/Controller/PageController.java
+++ src/main/java/com/huang/Controller/PageController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class PageController {
 
-    // 访问根路径时跳转到 index.html
+    // href="/"跳转页面
+	//访问根路径时跳转到 index.html
     @GetMapping("/")
     public String index() {
         return "hello"; // 返回视图名称 "index" -> src/main/resources/templates/hello.html
@@ -18,6 +19,10 @@ public class PageController {
     public String submitForm() {
         return "success"; // 返回视图名称 "success" -> src/main/resources/templates/success.html
     }
-    
-    
+    // window.location.href = "/success";跳转页面
+    @GetMapping("/success")
+    public String showSuccessPage() {
+        // 返回视图名称，对应的模板文件为 src/main/resources/templates/success.html
+        return "success";
+    }
 }
